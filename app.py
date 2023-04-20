973dbff43115e254b8ee4871a61667428b460953
StephenKiiyuruNganga <40911055+StephenKiiyuruNganga@users.noreply.github.com>
2023-04-20 08:39:20 +0300

    add softmax and loss demos

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ Notice that:
 left_col1, right_col1 = st.columns(2)
 
 with left_col1:
-    weight = st.slider("Weight1", -10.00, 10.00, 0.1)
-    bias = st.slider("Bias1", -10.00, 10.00, 0.1)
+    weight = st.slider("Weight1", -10.00, 10.00, 1.0, 0.1)
+    bias = st.slider("Bias1", -10.00, 10.00, 0.0, 0.1)
     output1 = ReLU(input * weight + bias)
 
 with right_col1:
@@ -54,12 +54,12 @@ for idx in range(2):
     left_col2, right_col2 = st.columns(2)
 
 with left_col2:
-    weight2 = st.slider("Weight2", -10.00, 10.00, 0.1)
-    bias2 = st.slider("Bias2", -10.00, 10.00, 0.1)
+    weight2 = st.slider("Weight2", -10.00, 10.00, 1.0, 0.1)
+    bias2 = st.slider("Bias2", -10.00, 10.00, 0.0, 0.1)
     output2 = ReLU(input * weight2 + bias2)
 
-    weight3 = st.slider("Weight3", -10.00, 10.00, 0.1)
-    bias3 = st.slider("Bias3", -10.00, 10.00, 0.1)
+    weight3 = st.slider("Weight3", -10.00, 10.00, -1.5, 0.1)
+    bias3 = st.slider("Bias3", -10.00, 10.00, 4.0, 0.1)
     output3 = ReLU(output2 * weight3 + bias3)
 
 with right_col2:
@@ -67,3 +67,32 @@ with right_col2:
     ax2.plot(input, output3)
     ax2.set_ylim(-1, 10)
     st.pyplot(fig2)
+
+"## 2. Softmax"
+"### Exponentaition: $y = e^{x}$"
+
+exp_values = np.exp(input)
+fig3, ax3 = plt.subplots()
+ax3.plot(input, exp_values)
+ax3.set_ylim(0, 1)
+st.pyplot(fig3)
+
+"## 3. Loss"
+
+st.markdown(
+    """
+- For classification problems, we use categorical cross-entropy.
+When we use one-hot encoding for our target labels, the equation
+becomes:
+$-log(targetClassProbability)$
+- Notice that when probability is high, loss is low and vice versa.
+"""
+)
+
+left_col3, right_col3 = st.columns(2)
+with left_col3:
+    y_pred = st.slider("Probability score", 0.1, 1.0, 0.9, 0.1)
+
+with right_col3:
+    loss = -np.log(y_pred)
+    f"Loss = {loss}"
